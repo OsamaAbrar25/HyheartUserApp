74afc4a57973be54ae05240ca1895fad45474b15
Vishesh Dubey <visheshdubey2016@gmail.com>
2023-12-05 09:46:34 +0530

    JWT null issue resolved

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,18 @@
-import { Avatar, Button, ListItem } from '@rneui/themed';
-import React, { useEffect, useRef, useState } from 'react';
-import { FlatList, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome6';
-import { PermissionsAndroid, Platform } from 'react-native';
-
-import * as ZIM from 'zego-zim-react-native';
-import * as ZPNs from 'zego-zpns-react-native';
+import { Avatar, Button, ListItem } from "@rneui/themed";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome6";
+import { PermissionsAndroid, Platform } from "react-native";
+import auth from "@react-native-firebase/auth";
+import * as ZIM from "zego-zim-react-native";
+import * as ZPNs from "zego-zpns-react-native";
 
 import ZegoUIKitPrebuiltCallService, {
   ZegoCallInvitationDialog,
@@ -14,36 +21,43 @@ import ZegoUIKitPrebuiltCallService, {
   ZegoSendCallInvitationButton,
   ZegoMenuBarButtonName,
   ZegoUIKitPrebuiltCallFloatingMinimizedView,
-} from '@zegocloud/zego-uikit-prebuilt-call-rn';
-import { useGetProvidersQuery, useGetZegoTokenQuery } from '../../apis/user';
-import { useSelector } from 'react-redux';
+} from "@zegocloud/zego-uikit-prebuilt-call-rn";
+import { useGetProvidersQuery, useGetZegoTokenQuery } from "../../apis/user";
+import { useSelector } from "react-redux";
 
 // import {ZegoUIKitPrebuiltCall, ONE_ON_ONE_VOICE_CALL_CONFIG } from '@zegocloud/zego-uikit-prebuilt-call-rn'
 
 const appID = 1249344658;
-const appSign = "2ed2bb743e506fe41e001c9a0db1bedb84a7134a6d6808ff544a640fdfa4d181";
+const appSign =
+  "2ed2bb743e506fe41e001c9a0db1bedb84a7134a6d6808ff544a640fdfa4d181";
 const userID = "1";
 const userName = "Test User";
 
 const Home = ({ navigation }) => {
-
-  const name = useSelector(state => state.auth.userData.name)
-  const photoURL = useSelector(state => state.auth.userData.photoURL)
-  const jwt2 = useSelector(state => state.auth.jwt)
+  const name = useSelector((state) => state.auth.userData.name);
+  const photoURL = useSelector((state) => state.auth.userData.photoURL);
+  const jwt2 = useSelector((state) => state.auth.jwt);
 
   const response = useGetZegoTokenQuery();
-  const providersListRes = useGetProvidersQuery()
+  const providersListRes = useGetProvidersQuery();
 
   // console.log("Zego LOG: ", response.data);
-  
-  
-  useEffect(()=> {
+
+  useEffect(() => {
     console.log("Providers List::::::::::::::::::::::::::", providersListRes);
     console.log("jwt2222222222", jwt2);
-    
-  }, [providersListRes])
+  }, [providersListRes]);
 
-
+  const handleLogout = async () => {
+    try {
+      await auth().signOut();
+      // Do something after successful logout, e.g., navigate to login screen
+      // navigation.navigate('Login'); // If using navigation
+    } catch (error) {
+      // Handle logout error
+      console.error("Error while logging out:", error);
+    }
+  };
   useEffect(() => {
     try {
       ZegoUIKitPrebuiltCallService.init(
@@ -54,17 +68,18 @@ const Home = ({ navigation }) => {
         [ZIM, ZPNs],
         {
           ringtoneConfig: {
-            incomingCallFileName: 'rutu.mp3',
-            outgoingCallFileName: 'ringing.mp3',
+            incomingCallFileName: "rutu.mp3",
+            outgoingCallFileName: "ringing.mp3",
           },
           notifyWhenAppRunningInBackgroundOrQuit: true,
           androidNotificationConfig: {
             channelID: "AudioChannel",
             channelName: "CC",
           },
-        })
+        }
+      );
     } catch (error) {
-      console.error('Error initializing ZegoUIKitPrebuiltCallService:', error);
+      console.error("Error initializing ZegoUIKitPrebuiltCallService:", error);
     }
   }, []);
 
@@ -72,77 +87,89 @@ const Home = ({ navigation }) => {
     <View style={{ backgroundColor: "white", height: "100%" }}>
       <View
         style={{
-          flexDirection: 'row',
+          flexDirection: "row",
           padding: 16,
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          backgroundColor: '#FADD9A',
+          alignItems: "center",
+          justifyContent: "space-between",
+          backgroundColor: "#FADD9A",
           marginBottom: 16,
-        }}>
-        <View style={{ flexDirection: 'row', gap: 16, alignItems: 'center' }}>
+        }}
+      >
+        <View style={{ flexDirection: "row", gap: 16, alignItems: "center" }}>
           <Avatar
             rounded
             // showEditButton
-            size={'medium'}
-            avatarStyle={{ borderWidth: 3, borderColor: 'white' }}
+            size={"medium"}
+            avatarStyle={{ borderWidth: 3, borderColor: "white" }}
             source={{
               uri: photoURL,
             }}
           />
           <View>
-            <Text style={{ fontWeight: 'bold', color: 'black' }}>Hi! {name}</Text>
-            <Text style={{ fontSize: 12, color: 'gray' }}>Your Credits: 12</Text>
+            <Text style={{ fontWeight: "bold", color: "black" }}>
+              Hi! {name}
+            </Text>
+            <Text style={{ fontSize: 12, color: "gray" }}>
+              Your Credits: 12
+            </Text>
           </View>
         </View>
         <Button
-          size={'sm'}
-          buttonStyle={{ paddingHorizontal: 20, backgroundColor: '#5E449B' }}
+          size={"sm"}
+          buttonStyle={{ paddingHorizontal: 20, backgroundColor: "#5E449B" }}
           containerStyle={{ borderRadius: 50 }}
           titleStyle={{ fontSize: 10 }}
-          title={'Buy Credits'}
-          onPress={() => navigation.navigate('BuyCredits')}
+          title={"Buy Credits"}
+          onPress={() => handleLogout()} //navigation.navigate('BuyCredits')}
         />
       </View>
 
       <View style={{ paddingHorizontal: 16 }}>
-        <Text style={{ color: 'black', fontWeight: '600' }}>Active Users</Text>
+        <Text style={{ color: "black", fontWeight: "600" }}>Active Users</Text>
       </View>
-
 
       <ListItem
         style={{
           paddingVertical: 0,
-        }}>
+        }}
+      >
         <Avatar
           rounded
-          source={{ uri: 'https://randomuser.me/api/portraits/men/36.jpg' }}
-          avatarStyle={{ borderWidth: 3, borderColor: 'gold' }}
+          source={{ uri: "https://randomuser.me/api/portraits/men/36.jpg" }}
+          avatarStyle={{ borderWidth: 3, borderColor: "gold" }}
           size={50}
         />
         <ListItem.Content>
-          <ListItem.Title style={{ fontWeight: '700', fontSize: 14 }}>
+          <ListItem.Title style={{ fontWeight: "700", fontSize: 14 }}>
             Osama Abrar
           </ListItem.Title>
           <ListItem.Subtitle style={{ fontSize: 11 }}>
             M | 18-25 Years
           </ListItem.Subtitle>
         </ListItem.Content>
-        <Icon name="user-large" size={25} color="#5E449B" onPress={() => navigation.navigate('UserProfile')} />
+        <Icon
+          name="user-large"
+          size={25}
+          color="#5E449B"
+          onPress={() => navigation.navigate("UserProfile")}
+        />
         <ZegoSendCallInvitationButton
-          invitees={[{
-            userID: "2", userName: "Provider User"
-          }]}
+          invitees={[
+            {
+              userID: "2",
+              userName: "Provider User",
+            },
+          ]}
           isVideoCall={false}
           resourceID={"zego_data"} // Please fill in the resource ID name that has been configured in the ZEGOCLOUD's console here.
         />
       </ListItem>
 
-
-      {providersListRes.isSuccess &&
+      {providersListRes.isSuccess && (
         <FlatList
           data={providersListRes.data}
-          renderItem={({ item }) => <Text>{item}</Text>
-          
+          renderItem={
+            ({ item }) => <Text>{JSON.stringify(item)}</Text>
 
             // <ListItem
             //   style={{
@@ -171,11 +198,10 @@ const Home = ({ navigation }) => {
             //     resourceID={"zego_data"} // Please fill in the resource ID name that has been configured in the ZEGOCLOUD's console here.
             //   />
             // </ListItem>
-
           }
-          keyExtractor={item => item.id}
+          keyExtractor={(item) => item.id}
         />
-      }
+      )}
 
       {/* <ZegoUIKitPrebuiltCall
         appID={appID}
@@ -199,16 +225,16 @@ const styles = StyleSheet.create({
   button: {
     paddingHorizontal: 25,
     paddingVertical: 4,
-    fontWeight: 'bold',
-    color: '#ffffff',
-    backgroundColor: '#0055cc',
+    fontWeight: "bold",
+    color: "#ffffff",
+    backgroundColor: "#0055cc",
     margin: 5,
   },
-  main: { flex: 1, alignItems: 'center' },
-  scroll: { flex: 1, backgroundColor: '#ddeeff', width: '100%' },
-  scrollContainer: { alignItems: 'center' },
-  videoView: { width: '90%', height: 200 },
-  btnContainer: { flexDirection: 'row', justifyContent: 'center' },
+  main: { flex: 1, alignItems: "center" },
+  scroll: { flex: 1, backgroundColor: "#ddeeff", width: "100%" },
+  scrollContainer: { alignItems: "center" },
+  videoView: { width: "90%", height: 200 },
+  btnContainer: { flexDirection: "row", justifyContent: "center" },
   head: { fontSize: 20 },
 });
 
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,137 +1,157 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text } from 'react-native';
-import { Button } from '@rneui/themed';
-import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
-import { useValidateFirebaseTokenMutation } from '../apis/user';
-import LinearGradient from 'react-native-linear-gradient';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useDispatch } from 'react-redux';
-import { storeJwt, storeUserData } from '../store/slices/authSlice';
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { Button } from "@rneui/themed";
+import {
+  GoogleSignin,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+import auth from "@react-native-firebase/auth";
+import { useValidateFirebaseTokenMutation } from "../apis/user";
+import LinearGradient from "react-native-linear-gradient";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useDispatch, useSelector } from "react-redux";
+import { storeJwt, storeUserData } from "../store/slices/authSlice";
 
 const SignIn = ({ navigation }) => {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState(null);
+  const [validateFirebaseToken, validateFirebaseTokenRes] =
+    useValidateFirebaseTokenMutation();
+  const dispatch = useDispatch();
+  const jwt2 = useSelector((state) => state.auth.jwt);
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        "464645337735-ffkhbmdnod8uc9084pvtj0di82ins1tk.apps.googleusercontent.com",
+    });
+  }, []);
 
-    // Set an initializing state whilst Firebase connects
-    const [initializing, setInitializing] = useState(true);
-    const [user, setUser] = useState(null);
-    const [validateFirebaseToken, validateFirebaseTokenRes] = useValidateFirebaseTokenMutation();
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        GoogleSignin.configure({
-            webClientId: '464645337735-ffkhbmdnod8uc9084pvtj0di82ins1tk.apps.googleusercontent.com',
-        });
-    }, []);
-
-    useEffect(() => {
-        if (validateFirebaseTokenRes.isSuccess) {
-            dispatch(storeJwt(validateFirebaseTokenRes.data.jwt));
-            console.log("JWT333333333333333333333😊 " + JSON.stringify(validateFirebaseTokenRes.data.jwt));
-        }
-    }, [validateFirebaseTokenRes.isSuccess]);
-
-    // Handle user state changes
-    function onAuthStateChanged(user) {
-        setUser(user);
-        if (initializing) setInitializing(false);
-    }
-
-    useEffect(() => {
-        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-        return subscriber; // unsubscribe on unmount
-    }, []);
-
-
-    const onGoogleSignIn = async () => {
-        try {
-            await GoogleSignin.hasPlayServices();
-            const userInfo = await GoogleSignin.signIn();
-            const googleCredential = auth.GoogleAuthProvider.credential(userInfo.idToken);
-            // await auth().signInWithCredential(googleCredential);
-            const user = auth().currentUser;
-
-            const response = await auth().signInWithCredential(googleCredential)
-
-            // console.log("00000000000000000000000000", response);
-
-            // .then((result) => {
-            //     result.user
-            //         .getIdToken()
-            //         .then((token) => {
-            //             console.log("Token: ", token);
-            //             validateFirebaseToken({ token: token })
-            //                 .then((res) => dispatch(storeJwt(res.data.jwt))
-            //                 )
-            //         })
-            //     console.log(JSON.stringify(result.user));
-            //     dispatch(storeUserData({
-            //         name: result.user.displayName,
-            //         photoURL: result.user.photoURL,
-            //         email: result.user.email,
-            //     }))
-
-            // });
-
-            const token = await response.user.getIdToken();
-            await validateFirebaseToken({ token });
-            console.log("00000000000000000000000000", token);
-            // if (validateFirebaseTokenRes.isSuccess) {
-                dispatch(storeJwt(validateFirebaseTokenRes.data.jwt));
-                console.log("JWT333333333333333333333😊 " + JSON.stringify(validateFirebaseTokenRes));
-            // }
-
-            // console.log(JSON.stringify(response.user.getIdTokenResult()));
-
-
-        } catch (error) {
-            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-                // User canceled the sign-in process
-                console.log(error);
-
-            } else if (error.code === statusCodes.IN_PROGRESS) {
-                // Operation (e.g., sign in) is in progress already
-                console.log(error);
-            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-                // Play services not available or outdated
-                console.log(error);
-            } else {
-                // Some other error happened
-                console.log(error);
-            }
-        }
-    };
-
-    if (initializing) return null;
-
+  useEffect(() => {
     if (validateFirebaseTokenRes.isSuccess) {
-        console.log("JWT333333333333333333333 " + JSON.stringify(validateFirebaseTokenRes.data.jwt));
-        dispatch(storeJwt(validateFirebaseTokenRes.data.jwt));
-        // AsyncStorage.setItem('JWT', validateFirebaseTokenRes.data.jwt);
+      dispatch(storeJwt(validateFirebaseTokenRes.data.jwt));
+      console.log(
+        "JWT333333333333333333333😊 " +
+          JSON.stringify(validateFirebaseTokenRes.data.jwt)
+      );
     }
+  }, [validateFirebaseTokenRes.isSuccess]);
 
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+  const handleLogout = async () => {
+    try {
+      await auth().signOut();
+      // Do something after successful logout, e.g., navigate to login screen
+      // navigation.navigate('Login'); // If using navigation
+    } catch (error) {
+      // Handle logout error
+      console.error("Error while logging out:", error);
+    }
+  };
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
 
-    return (
-        <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
-            {user ?
-                navigation.reset({
-                    index: 0,
-                    routes: [{ name: 'MainTabNavigator' }],
-                })
-                :
-                <Button
-                    ViewComponent={LinearGradient} // Don't forget this!
-                    linearGradientProps={{
-                        colors: ['#FF84A7', '#E03368'],
-                        start: { x: 0, y: 0.5 },
-                        end: { x: 1, y: 0.5 },
-                    }}
-                    buttonStyle={{ width: 200, borderRadius: 4 }}
-                    title="Sign in with Google"
-                    onPress={onGoogleSignIn}
-                />
-            }
-        </View>
-    );
-}
+  const onGoogleSignIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      const googleCredential = auth.GoogleAuthProvider.credential(
+        userInfo.idToken
+      );
+      const user = auth().currentUser;
 
-export default SignIn
+      const response = await auth().signInWithCredential(googleCredential);
+
+      // console.log("00000000000000000000000000", response);
+
+      // .then((result) => {
+      //     result.user
+      //         .getIdToken()
+      //         .then((token) => {
+      //             console.log("Token: ", token);
+      //             validateFirebaseToken({ token: token })
+      //                 .then((res) => dispatch(storeJwt(res.data.jwt))
+      //                 )
+      //         })
+      //     console.log(JSON.stringify(result.user));
+      //     dispatch(storeUserData({
+      //         name: result.user.displayName,
+      //         photoURL: result.user.photoURL,
+      //         email: result.user.email,
+      //     }))
+
+      // });
+
+      const token = await response.user.getIdToken();
+      const validateTokenResponse = await validateFirebaseToken({ token });
+      //   console.log("00000000000000000000000000", token);
+      console.log("🍎🍎 ", JSON.stringify(validateTokenResponse));
+      if (validateTokenResponse.data) {
+        dispatch(storeJwt(validateTokenResponse.data.jwt));
+        console.log("JWT333333333333333333333😊 " + JSON.stringify(jwt2));
+      } else {
+        console.error("Failed to validate Firebase token");
+      }
+
+      // console.log(JSON.stringify(response.user.getIdTokenResult()));
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // User canceled the sign-in process
+        console.log(error);
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // Operation (e.g., sign in) is in progress already
+        console.log(error);
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // Play services not available or outdated
+        console.log(error);
+      } else {
+        // Some other error happened
+        console.log(error);
+      }
+    }
+  };
+
+  if (initializing) return null;
+
+  //   if (validateFirebaseTokenRes.isSuccess) {
+  //     console.log(
+  //       "JWT333333333333333333333 " +
+  //         JSON.stringify(validateFirebaseTokenRes.data.jwt)
+  //     );
+  //     dispatch(storeJwt(validateFirebaseTokenRes.data.jwt));
+  //     // AsyncStorage.setItem('JWT', validateFirebaseTokenRes.data.jwt);
+  //   }
+
+  return (
+    <View
+      style={{ height: "100%", justifyContent: "center", alignItems: "center" }}
+    >
+      {user ? (
+        navigation.reset({
+          index: 0,
+          routes: [{ name: "MainTabNavigator" }],
+        })
+      ) : (
+        <Button
+          ViewComponent={LinearGradient} // Don't forget this!
+          linearGradientProps={{
+            colors: ["#FF84A7", "#E03368"],
+            start: { x: 0, y: 0.5 },
+            end: { x: 1, y: 0.5 },
+          }}
+          buttonStyle={{ width: 200, borderRadius: 4 }}
+          title="Sign in with Google"
+          onPress={onGoogleSignIn}
+        />
+      )}
+    </View>
+  );
+};
+
+export default SignIn;
